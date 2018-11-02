3d3bcafd2d920802d72b29383dfa67c85eb9221d
Isa Notebook <isamed92@gmail.com>
2018-11-02 00:19:57 -0300

    added all dialog, tooltip and snackbar examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,39 @@ import 'hammerjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatDialogModule, MatCardModule, MatTooltipModule, MatSnackBarModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { MyFormComponent } from './my-form/my-form.component';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+import { ToolTipDemoComponent } from './tool-tip-demo/tool-tip-demo.component';
+import { SnackbarDemoComponent } from './snackbar-demo/snackbar-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFormComponent
+    MyFormComponent,
+    DialogDemoComponent,
+    MyDialogComponent,
+    ToolTipDemoComponent,
+    SnackbarDemoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSnackBarModule
+  ],
+  entryComponents: [
+    MyDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
